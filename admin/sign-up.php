d8c38ed5669d5cddff597b52d9fcc3c7f4dc3ad7
Brendan Rogers <brendanr11@gmail.com>
2019-02-11 10:52:16 -0500

    Integrated Mailchimp API

--- admin/sign-up.php
+++ admin/sign-up.php
@@ -7,25 +7,24 @@ function signUp($firstname, $lastname, $email, $country) {
 	$check_email_query = 'SELECT COUNT(*) FROM tbl_users WHERE email = :email';
 	$check_email_set = $pdo->prepare($check_email_query);
 	$check_email_set->execute(
-		array(
-			':email'=>$email
-		)
+		array( ':email'=>$email )
 	);
+
 	// do something special if the users email already exists in the system
 	if($check_email_set->fetchColumn() > 0) {
-		// update the users last update column
 
+		// update the users last update column
 		$update_last_query = 'UPDATE tbl_users SET last_update = current_timestamp WHERE email = :email';
 		$update_last_set = $pdo->prepare($update_last_query);
 		$update_last_set->execute(
-			array(
-				':email'=>$email
-			)
+			array( ':email'=>$email )
 		);
 		return sendEmail($firstname, $email, true);
-		exit;
-
 	} else {
+
+		// add them to the mailing list
+		mailchimp($firstname, $lastname, $email);
+
 		// insert user information
 		$insert_user_query = "INSERT INTO tbl_users(firstname, lastname, email, country) VALUES (:firstname, :lastname, :email, :country)";
 		$insert_user_set = $pdo->prepare($insert_user_query);
@@ -56,6 +55,35 @@ function sendEmail($name, $email, $returning) {
 }
 
 
+
+
+function mailchimp($firstname, $lastname, $email) {
+	$authToken = 'f944a226463b32b5cd866e55fac89956-us20';
+	$list_id = 'eeddad44e9';
+ 	// The data to send to the API
+	$postData = array(
+	    "email_address" => $email, 
+	    "status" => "subscribed", 
+	    "merge_fields" => array(
+		    "FNAME"=> $firstname,
+		    "LNAME"=> $lastname
+		)
+	);
+
+	// Setup cURL
+	$ch = curl_init('https://us20.api.mailchimp.com/3.0/lists/'.$list_id.'/members/');
+	curl_setopt_array($ch, array(
+	    CURLOPT_POST => TRUE,
+	    CURLOPT_RETURNTRANSFER => TRUE,
+	    CURLOPT_HTTPHEADER => array(
+	        'Authorization: apikey '.$authToken,
+	        'Content-Type: application/json'
+	    ),
+	    CURLOPT_POSTFIELDS => json_encode($postData)
+	));
+	// Send the request
+	$response = curl_exec($ch);
+}
 
 
 ?>
