6491a133fc8adfa5b6536960ef05c3e59c1bc621
christine193 <35804233+christine193@users.noreply.github.com>
2019-02-12 01:00:07 -0500

    cleaned up

--- js/main.js
+++ js/main.js
@@ -1,3 +1,40 @@
+
+
+var target = document.querySelectorAll('.scroll');
+function parallax(){
+   //document.getElementsByClassName('transition');
+//target.style.color = '#efefef';
+
+//var scrolled = window.pageYOffset;
+//var rate = scrolled * 0.3;
+
+//target.style.transform = 'translate3d(0px, '+rate+'px , 0px)' ; //rate is determined by the amount scrolled
+
+  
+if( index = 0 ) {
+    length = target.length;
+} 
+
+for(index; index < length; index ++) { //incramenting lenght
+    var position = window.pageYOffset * target [index].dataset.rate;
+    
+  target[index].style.transform = 'translate3d(0px, '+position+'px , 0px)' ;
+
+
+}
+
+function colorChange(){
+
+}
+
+};
+
+
+window.addEventListener("scroll" , parallax);
+
+
+
+
 // components will go here
 import homeComponent from './components/homeComponent.js'; // this is like doing a php include 
 import signupComponent from './components/signupComponent.js';   
@@ -19,4 +56,5 @@ const vm = new Vue({
     router: router
     
 }).$mount("#app");
+
 
